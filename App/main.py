52bb29ba55563aa8d1cf9507c3f574605e3f19a9
Kripesh Nihure <nkripesh0@gmail.com>
2024-12-31 00:21:35 +0545

    requirements added for deployment

--- App/main.py
+++ App/main.py
@@ -1,24 +1,30 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 def main():
     st.set_page_config(page_title="NLP-Based DBMS",initial_sidebar_state='collapsed',menu_items={
         'About': "# This is a header. This is an *extremely* cool app!"
     })
-    st.title("NLP Based DBMS")
-    
-    # Promotional and informative content
+
+    c1,c2 = st.columns(2)
+    with c2:
+        st.text('')
+        st.text('') 
+        st.text('') 
+        st.text('') 
+        st.text('') 
+        st.title("\n\nNLP Based DBMS")
+        st.write(""" A natural language interface to SQL databases.""")
+    with c1:
+        st_lottie("https://lottie.host/67ccc844-7aeb-4d60-9eec-4f8ff8c12dfc/82SezLULZG.json", height=400, width=300,loop=True)
+
+# Promotional and informative content
     st.header("About Our Project")
     st.write("""
         Our NLP-based DBMS simplifies database querying by allowing users to input natural language queries.
         It interprets the queries, processes them, and returns accurate results from the SQL database.
     """)
 
-    st.header("Meet the Developers")
-    st.write("""
-        - Avahan Tamrahkar [Application developer]
-        - Javed Ansari [NLP engineer]
-        - Kripesh Nihure [Web developer]
-    """)
 
     st.header("Key Features")
     st.markdown("""
@@ -33,7 +39,13 @@ def main():
         2. Input your query in English.
         3. Get results instantly!
     """)
-
+    
+    st.header("Meet the Developers")
+    st.write("""
+        - Avahan Tamrahkar [Application developer]
+        - Javed Ansari [NLP engineer]
+        - Kripesh Nihure [Web developer]
+    """)
     st.markdown("""
         ### Let's get started by navigating to the Configuration page!
     """)
